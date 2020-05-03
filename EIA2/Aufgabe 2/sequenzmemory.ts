4702d7e2614c509294adbd31d0fb87c51ffbc2a7
MareikeMe <mareike.mezger@hs-furtwangen.de>
2020-05-03 17:09:55 +0200

    aufgabe 2 abgabe

--- EIA2/Aufgabe 2/sequenzmemory.ts
+++ EIA2/Aufgabe 2/sequenzmemory.ts
@@ -1,12 +1,25 @@
 namespace Aufgabe2SequenceMemory {
 
+window.addEventListener("load", function (): void{
+    this.document.querySelector("document").addEventListener("keydown", function (): void {});
+    this.document.querySelector("#start").addEventListener("click", function(): void {});
+    this.document.querySelector("#card").addEventListener("click", function(): void {});
+});
 
+function sequences (): void {
+    var sentenceone: string [] = ["S", "e", "n", "t", "e", "n", "c", "e", " ", "o", "n", "e", "."];
 
+}
 
+function time (): void {
+    var onemin: string [] = ["60", "59", "58"];
+    var index: number = 0;
+    var countdown = setInterval (1000);
 
+    function playcountdown (): void {
 
+        index++;
+    }
+}
 
-
-
-    
 }
