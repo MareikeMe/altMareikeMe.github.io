b7f7149f40d9ff3a9b5b88df8c9c9119fd2a15cf
MareikeMe <mareike.mezger@hs-furtwangen.de>
2020-05-09 23:18:23 +0200

    code and diagrams

--- EIA2/Aufgabe3/Haushaltshilfe.js
+++ EIA2/Aufgabe3/Haushaltshilfe.js
@@ -6,25 +6,20 @@ var Haushaltshilfe;
         var form = document.querySelector("div#form");
         var slider = document.querySelector("input#sqmmow");
         form.addEventListener("change", handleChange);
-        slider.addEventListener("input", displayAmount);
+        slider.addEventListener("input", displaySquarmeters);
     }
     function handleChange(_event) {
-        // console.log(_event);
-        // let drink: HTMLSelectElement = <HTMLSelectElement>document.querySelector("select");
-        // console.log(drink.value);
-        // let inputs: NodeListOf<HTMLInputElement> = document.querySelectorAll("input");
-        // console.log(inputs);
-        var order = document.querySelector("div#order");
-        order.innerHTML = "";
+        var cart = document.querySelector("div#cart");
+        cart.innerHTML = "";
         var formData = new FormData(document.forms[0]);
         for (var _i = 0, formData_1 = formData; _i < formData_1.length; _i++) {
             var entry = formData_1[_i];
-            var item = document.querySelector("[value='" + entry[1] + "']");
-            var price = Number(item.getAttribute("price"));
-            order.innerHTML += item.name + "  € " + price;
+            var article = document.querySelector("[value='" + article[1] + "']");
+            var price = Number(article.getAttribute("price"));
+            cart.innerHTML += article.name + "  € " + price;
         }
     }
-    function displayAmount(_event) {
+    function displaySquarmeters(_event) {
         var progress = document.querySelector("progress");
         var amount = _event.target.value;
         progress.value = parseFloat(amount);
--- EIA2/Aufgabe3/Haushaltshilfe.ts
+++ EIA2/Aufgabe3/Haushaltshilfe.ts
@@ -7,30 +7,25 @@ namespace Haushaltshilfe {
         let slider: HTMLInputElement = <HTMLInputElement>document.querySelector("input#sqmmow");
 
         form.addEventListener("change", handleChange);
-        slider.addEventListener("input", displayAmount);
+        slider.addEventListener("input", displaySquarmeters);
     }
 
     function handleChange(_event: Event): void {
-        // console.log(_event);
-        // let drink: HTMLSelectElement = <HTMLSelectElement>document.querySelector("select");
-        // console.log(drink.value);
+       
 
-        // let inputs: NodeListOf<HTMLInputElement> = document.querySelectorAll("input");
-        // console.log(inputs);
-
-        let order: HTMLDivElement = <HTMLDivElement>document.querySelector("div#order");
-        order.innerHTML = "";
+        let cart: HTMLDivElement = <HTMLDivElement>document.querySelector("div#cart");
+        cart.innerHTML = "";
 
         let formData: FormData = new FormData(document.forms[0]);
         for (let entry of formData) {
-            let item: HTMLInputElement = <HTMLInputElement>document.querySelector("[value='" +  entry[1] + "']");
-            let price: number = Number(item.getAttribute("price"));
+            let article: HTMLInputElement = <HTMLInputElement>document.querySelector("[value='" +  article[1] + "']");
+            let price: number = Number(article.getAttribute("price"));
 
-            order.innerHTML += item.name + "  € " + price;
+            cart.innerHTML += article.name + "  € " + price;
         }
     }
 
-    function displayAmount(_event: Event): void {
+    function displaySquarmeters(_event: Event): void {
         let progress: HTMLProgressElement = <HTMLProgressElement>document.querySelector("progress");
         let amount: string = (<HTMLInputElement>_event.target).value;
         progress.value = parseFloat(amount);
